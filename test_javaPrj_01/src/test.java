e39d9268c5d3451f1c9a0fb42e677198722fb0d8
khc <vscode_test@gmail.com>
2023-03-26 22:41:02 +0900

    로컬 푸쉬2

--- test_javaPrj_01/src/test.java
+++ test_javaPrj_01/src/test.java
@@ -9,5 +9,7 @@ public class test {
         System.out.println("연동 여부 확인");
         System.out.println("온라인에서 수정 후 커밋");
         System.out.println("재차 온라인 수정 푸쉬");
+        System.out.println("내 로컬에서 동일 소스 수정 상황");
+
     }
 }
