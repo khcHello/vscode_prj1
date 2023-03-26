ad9e6c382220455e590e2556d1e7838e2e5d28ff
김희철 <47128504+khcGood@users.noreply.github.com>
2023-03-26 22:40:17 +0900

    2번째 테스트

--- test_javaPrj_01/src/test.java
+++ test_javaPrj_01/src/test.java
@@ -8,5 +8,6 @@ public class test {
         System.out.println("Hello, VS Code 2");
         System.out.println("연동 여부 확인");
         System.out.println("온라인에서 수정 후 커밋");
+        System.out.println("재차 온라인 수정 푸쉬");
     }
 }
