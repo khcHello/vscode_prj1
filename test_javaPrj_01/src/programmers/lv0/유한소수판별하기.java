36b3b891b9226de1f93e8cdd600188b599c61f8b
khcGood <47128504+khcGood@users.noreply.github.com>
2023-03-27 18:26:33 +0900

    알고리즘 진행 커밋 230327

---
+++ test_javaPrj_01/src/programmers/lv0/유한소수판별하기.java
@@ -0,0 +1,62 @@
+package src.programmers.lv0;
+
+public class 유한소수판별하기 {
+    
+    public void Solution() throws Exception{
+        
+        int a = 12;
+        int b = 21;
+
+        int greatestCommonDivisor = getTheGreatestCommonDivisor(a, b);
+        
+        int reduceDenominator = b/greatestCommonDivisor; 
+        
+        int answer = 1;
+        int dividor = 2;
+        while( reduceDenominator > 1 ){
+            if(reduceDenominator%dividor == 0){
+                if(!(dividor == 2 || dividor == 5)){
+                    answer = 2;
+                    break;
+                }
+                reduceDenominator /= dividor;
+            }else{
+                dividor++;
+            }
+        }
+        System.out.println(answer);
+        
+        // return answer;
+
+    }
+
+
+    public int getTheGreatestCommonDivisor(int numerator, int denominator){
+
+        while(denominator != 0){
+            int remain = numerator % denominator;
+            numerator = denominator;
+            denominator = remain;
+        }
+
+        return numerator;
+    }
+
+    
+
+
+
+    public static void main(String[] args) {
+        
+        try{
+            new 유한소수판별하기().Solution();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+}
---
+++ test_javaPrj_01/src/programmers/lv0/특이한정렬.java
@@ -0,0 +1,87 @@
+package src.programmers.lv0;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class 특이한정렬 {
+
+    public void Solution() throws Exception{
+        
+        int[] numlist = {1, 2, 3, 4, 5, 6};
+        int n = 4;
+
+        List<UnusualSortObj> list = new ArrayList<>();
+        for(int num : numlist){
+            UnusualSortObj newObj = null;
+
+            if(num > n){
+                newObj = new UnusualSortObj(num, Math.abs((num - n)*2) );
+            }else{
+                newObj = new UnusualSortObj(num, Math.abs((num - n)*2) + 1 );
+            }
+            
+            list.add(newObj);
+        }
+        
+        Collections.sort(list);
+
+        // for(UnusualSortObj obj : list){
+        //     System.out.println(obj.getIdNumber());
+        // }
+        // return list.stream().mapToInt(obj -> obj.getIdNumber()).toArray();
+
+    }
+
+    public class UnusualSortObj implements Comparable<UnusualSortObj>{
+
+        private int idNumber;
+        private int distance;
+
+        public UnusualSortObj(int idNumber, int distance){
+            this.idNumber = idNumber;
+            this.distance = distance;
+        }
+
+        public int getIdNumber() {
+            return this.idNumber;
+        }
+
+        public void setIdNumber(int idNumber) {
+            this.idNumber = idNumber;
+        }
+
+        public int getDistance() {
+            return this.distance;
+        }
+
+        public void setDistance(int distance) {
+            this.distance = distance;
+        }
+
+        @Override
+        public int compareTo(UnusualSortObj o) {
+            if (this.distance > o.distance) {
+                return 1;
+            }
+            if (this.distance < o.distance) {
+                return -1;
+            }
+            return 0;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        
+        try{
+
+            new 특이한정렬().Solution();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+    
+}
