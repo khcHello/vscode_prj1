de368b02eb5463c2d378e7f2accda542983a2797
khcGood <47128504+khcGood@users.noreply.github.com>
2023-03-28 13:30:41 +0900

    추가 알고리즘 230328

---
+++ test_javaPrj_01/src/programmers/lv0/저주의숫자3.java
@@ -0,0 +1,44 @@
+package src.programmers.lv0;
+
+public class 저주의숫자3 {
+    
+    public void Solution() throws Exception{
+
+        int n = 40;
+
+        int cursedCount = 0;
+        for(int i = 1; i < n+1; i++){
+
+            while(isCursed(i+cursedCount)){
+                cursedCount++;
+            }
+        }
+        
+        //return n + cursedCount;
+
+
+    }
+
+    public boolean isCursed(int i){
+
+        if(String.valueOf(i).indexOf("3") > -1 || i % 3 == 0){
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        
+        try{
+            new 저주의숫자3().Solution();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
---
+++ test_javaPrj_01/src/programmers/lv0/평행.java
@@ -0,0 +1,43 @@
+package src.programmers.lv0;
+
+import java.util.Arrays;
+
+public class 평행 {
+
+
+    public void Solution() throws Exception{
+        
+        // 기울기
+        // y증가량 / x증가량
+
+        // int[][] dots = {{1, 4}, {9, 2}, {3, 8}, {11, 6}};
+        int[][] dots = {{3, 5}, {4, 1}, {2, 4}, {6, 11}};
+
+        int[] x = dots[0];
+        int[] y = dots[1];
+        int[] a = dots[2];
+        int[] b = dots[3];
+
+        boolean result = (double)Math.abs(a[1] - b[1]) / (double)Math.abs(a[0] - b[0]) == (double)Math.abs(x[1] - y[1]) / (double)Math.abs(x[0] - y[0]);
+
+        // return result ? 1 : 0;
+    }
+
+    
+
+
+
+    public static void main(String[] args) {
+        
+        try{
+            new 평행().Solution();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
+
+
