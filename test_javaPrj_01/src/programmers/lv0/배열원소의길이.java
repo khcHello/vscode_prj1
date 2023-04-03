a86413aab25ab50121f8fa8f00f080648edce37d
khcGood <47128504+khcGood@users.noreply.github.com>
2023-04-03 11:25:11 +0900

    230403 하나더

---
+++ test_javaPrj_01/src/programmers/lv0/배열원소의길이.java
@@ -0,0 +1,33 @@
+package src.programmers.lv0;
+
+import java.util.Arrays;
+
+public class 배열원소의길이 {
+    
+    private 배열원소의길이(){
+        try{
+            Solution();
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public void Solution() throws Exception{
+
+        String[] strlist = {"We", "are", "the", "world!"};
+
+        int[] list = Arrays.stream(strlist).mapToInt(str -> str.length()).toArray();
+
+        System.out.println(Arrays.toString(list));
+
+    }
+
+
+    public static void main(String[] args) {
+        new 배열원소의길이();
+    }
+
+
+}
