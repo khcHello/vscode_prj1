e9386b03b4d660460c5addfd4cb5d951a7a79e12
khcGood <47128504+khcGood@users.noreply.github.com>
2023-04-05 12:30:44 +0900

    230405 진행

---
+++ test_javaPrj_01/src/programmers/lv0/문자열정렬하기1.java
@@ -0,0 +1,37 @@
+package src.programmers.lv0;
+
+import java.util.Arrays;
+
+public class 문자열정렬하기1 {
+    
+    private 문자열정렬하기1(){
+        try{
+            Solution();
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public void Solution() throws Exception{
+
+        String my_string = "hi12392";
+
+        String[] arr = my_string.replaceAll("[a-zA-Z]", "").split("");
+        Arrays.sort(arr);
+
+        // return Arrays.stream(arr).mapToInt(obj -> Integer.valueOf(obj)).toArray();
+        
+        
+        // System.out.println(String.join("", arr));
+
+    }
+
+
+    public static void main(String[] args) {
+        new 문자열정렬하기1();
+    }
+
+
+}
