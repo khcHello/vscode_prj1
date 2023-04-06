71edf325faa72aae20f68bc53d2c895a004a1610
khcGood <47128504+khcGood@users.noreply.github.com>
2023-04-06 15:12:32 +0900

    230406 진행

---
+++ test_javaPrj_01/src/programmers/lv0/모음제거.java
@@ -0,0 +1,27 @@
+package src.programmers.lv0;
+
+public class 모음제거 {
+    
+    private 모음제거(){
+        try{
+            Solution();
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public void Solution() throws Exception{
+
+        String answer = "";
+        // return my_string.replaceAll("a|e|i|o|u", "");
+    }
+
+
+    public static void main(String[] args) {
+        new 모음제거();
+    }
+
+
+}
---
+++ test_javaPrj_01/src/programmers/lv0/팩토리얼.java
@@ -0,0 +1,26 @@
+package src.programmers.lv0;
+
+public class 팩토리얼 {
+    
+    private 팩토리얼(){
+        try{
+            Solution();
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public void Solution() throws Exception{
+
+        
+    }
+
+
+    public static void main(String[] args) {
+        new 팩토리얼();
+    }
+
+
+}
