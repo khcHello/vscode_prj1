0b105632dd2188de130be842bd34e1a10ba88028
khcGood <47128504+khcGood@users.noreply.github.com>
2023-04-07 10:00:05 +0900

    230407 진행

--- test_javaPrj_01/src/programmers/lv0/팩토리얼.java
+++ test_javaPrj_01/src/programmers/lv0/팩토리얼.java
@@ -1,5 +1,7 @@
 package src.programmers.lv0;
 
+import src.test;
+
 public class 팩토리얼 {
     
     private 팩토리얼(){
@@ -14,9 +16,61 @@ public class 팩토리얼 {
     @SuppressWarnings("unused")
     public void Solution() throws Exception{
 
+        int n = 7;
+
+        int factorialValue = 1;
+
+        Factorial facLast = new Factorial();
+        Factorial facPrevious = new Factorial();
+
+        for(int i = 1; factorialValue-1 < n; i++){
+
+            facPrevious.setFactorial(i - 1);
+            facPrevious.setIntValue(factorialValue);
+
+            factorialValue *= i;
+            facLast.setFactorial(i);
+            facLast.setIntValue(factorialValue);
+        }
+       
+        System.out.println(facPrevious);
+        System.out.println(facLast);
+
+        //
+        int test = facLast.getIntValue() > n ? facPrevious.getFactorial() : facLast.getFactorial();
         
+        System.out.println(test);
     }
 
+    class Factorial{
+        private int factorial;
+        private int intValue;
+
+        private Factorial(){
+
+        }
+
+        public int getFactorial() {
+            return this.factorial;
+        }
+
+        public void setFactorial(int factorial) {
+            this.factorial = factorial;
+        }
+
+        public int getIntValue() {
+            return this.intValue;
+        }
+
+        public void setIntValue(int intValue) {
+            this.intValue = intValue;
+        }
+
+        @Override
+        public String toString() {
+            return this.factorial + " : "+this.intValue;
+        }
+    }
 
     public static void main(String[] args) {
         new 팩토리얼();
